1c779b53bd74e5dedc44254299c1f1db59400d2b
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-18 10:03:26 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <memory>
+#include <iostream>
+#include <memory.h>
+
+int main()
+{
+	printf("Now Your Game Should Work Fine With No problems, make sure to keep this opened untill you close your game");
+	system("pause");
+	return 0;
+
+}
