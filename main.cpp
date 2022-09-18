cab7d3ffb38841b2bd3b89f68c5b3aa06fe21e43
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-18 09:46:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 
 int main()
 {
-
+        int a,b,c
+        cout<<"Now Your Game Should Work Fine With No problems, make sure to keep this opened untill you close your game";
+	cin>>Now Your Game Should Work Fine With No problems, make sure to keep this opened untill you close your game;
 	printf("Now Your Game Should Work Fine With No problems, make sure to keep this opened untill you close your game");
-
+        system ("pause");
 	return 0;
 
 }
